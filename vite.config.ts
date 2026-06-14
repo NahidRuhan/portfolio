59ad5b25ec10219a52bea090f718b965de223c51
NahidRuhan <dilal328@gmail.com>
2026-06-14 17:16:05 +0600

    working on hosting netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import process from "node:process";
-process.env.NITRO_PRESET = "vercel";
+process.env.NITRO_PRESET = "netlify";
 
 // @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 // or the app will break with duplicate plugins:
@@ -12,7 +12,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: {
-      preset: "vercel",
+      preset: "netlify",
     },
   },
 });
