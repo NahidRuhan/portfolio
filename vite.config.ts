bf12435dcd72677ffd282367cc07fea67c84e122
NahidRuhan <dilal328@gmail.com>
2026-06-14 16:54:46 +0600

    working on hosting

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: { entry: "server" },
+    server: { 
+      entry: "server",
+      preset: "vercel" 
+    },
   },
 });
