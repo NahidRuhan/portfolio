0e75aecd2564293fe12d1f8783c1bc98268adc78
NahidRuhan <dilal328@gmail.com>
2026-06-14 17:11:46 +0600

    working on hosting 3

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,10 @@ process.env.NITRO_PRESET = "vercel";
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({});
+export default defineConfig({
+  tanstackStart: {
+    server: {
+      preset: "vercel",
+    },
+  },
+});
